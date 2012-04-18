0c71f7edffb17548044f6eda99dc42d76111997e
Zach Jones <zt.jones88@gmail.com>
2012-04-18 16:08:42 -0400

    Add user microposts

---
+++ app/controllers/microposts_controller.rb
@@ -0,0 +1,19 @@
+class MicropostsController < ApplicationController
+  before_filter :signed_in_user
+  
+  def create
+    @micropost = current_user.microposts.build(params[:micropost])
+    if @micropost.save
+      flash[:success]
+      redirect_to root_path
+    else
+      @feed_items = []
+      render 'static_pages/home'
+    end
+  end
+  
+  def destroy
+    @micropost.destroy
+    redirect_back_or root_path
+  end
+end
