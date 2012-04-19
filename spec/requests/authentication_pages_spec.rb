c3f54c9a542a7c38620abf0c8e0f10823d810293
Zach Jones <zt.jones88@gmail.com>
2012-04-19 16:08:01 -0400

    Add user following

--- spec/requests/authentication_pages_spec.rb
+++ spec/requests/authentication_pages_spec.rb
@@ -60,7 +60,7 @@ describe "Authentication" do
           fill_in "Password", with: user.password
           click_button "Sign in"
         end
-        
+
         describe "in the Microposts controller" do
           describe "submitting to the create action" do
             before { post microposts_path }
@@ -81,8 +81,20 @@ describe "Authentication" do
             page.should have_selector('title', text: 'Edit user')
           end
         end
+
+      describe "in the Relationships controller" do
+        describe "sibmitting to the create action" do
+          before { post relationships_path }
+          specify { response.should redirect_to(signin_path) }
+        end
+
+        describe "submitting to the destroy action" do
+          before { delete relationship_path(1) }
+          specify { response.should redirect_to(signin_path) }
+        end
       end
-      
+      end
+
       describe "as non-admin user" do
         let(:user) { FactoryGirl.create(:user) }
         let(:non_admin) { FactoryGirl.create(:user) }
@@ -105,7 +117,17 @@ describe "Authentication" do
         describe "visiting the user index" do
           before { visit users_path }
           it { should have_selector('title', text: 'Sign in') }
-        end        
+        end
+
+        describe "visiting the following page" do
+          before { visit following_user_path(user) }
+          it { should have_selector('title', text: 'Sign in') }
+        end
+
+        describe "visiting the followers page" do
+          before { visit followers_user_path(user) }
+          it { should have_selector('title', text: 'Sign in') }
+        end
         
         describe "submitting to the update action" do
           before { put user_path(user) }
